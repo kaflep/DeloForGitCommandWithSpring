36877b84668a18bba87b69cbeb8aa4d5f65d3346
pratik Kafle <118851897+kaflep@users.noreply.github.com>
2024-10-15 13:23:25 -0500

    Main class modified

--- src/main/java/com/example/GitDemo/GitDemoApplication.java
+++ src/main/java/com/example/GitDemo/GitDemoApplication.java
@@ -2,12 +2,15 @@ package com.example.GitDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitDemoApplication.class, args);
+
+		ConfigurableApplicationContext run =  SpringApplication.run(GitDemoApplication.class, args);
+		 run.close();
 	}
 
 }
